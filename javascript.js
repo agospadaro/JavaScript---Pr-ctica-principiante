b92fcd4a3c77d95074f8d51c278c3c0e15b8c942
agospadaro <you@example.com>
2019-10-23 22:03:24 -0300

    primer commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+let añoActual = 2019;
+
+const nombre = prompt("Indique su nombre");
+
+const año = prompt("Escriba su año de nacimiento - Formato AAAA");
+
+const mes = prompt("Escriba su mes de nacimiento - Formato MM");
+
+
+mes <= 10 && alert(`Su edad es ${2019 - año}`);
+
+mes > 10 && alert(`Su edad es ${2019 - año - 1}`);
